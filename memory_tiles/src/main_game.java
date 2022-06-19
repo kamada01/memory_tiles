45a68c104d90296fa7464584ef6f91d5212e4692
kam-ada <ada.kam208@gmail.com>
2022-06-20 00:23:38 +0800

    initialize

---
+++ memory_tiles/src/main_game.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.util.Random;
+
+public class main_game {
+	
+	//dimension of the grid
+	private int size;
+	
+	//number of tiles
+	private int no_tiles;
+	
+	//grid dimension UI
+	private int dimension;
+	
+	//foreground color;
+	private static final Color FOREGROUND_COLOR = new Color(1, 0, 0);
+	
+	//random object to generate pattern
+	private static final Random RANDOM = new Random();
+	
+	//Array of tiles
+	private int[] tiles;
+	
+	//size of tile UI
+	private int sz_tile;
+}
